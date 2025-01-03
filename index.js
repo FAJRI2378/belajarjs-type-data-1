0ea577d8ca2d86631577c7597a4cb3534d7dee94
FAJRI2378 <armanfajri008@gmail.com>
2025-01-03 21:55:06 +0700

    first commit

---
+++ index.js
@@ -0,0 +1,49 @@
+
+//type data primitive
+
+//string
+const nama = "fajri"
+const alamat = "{nama} jakarta"
+//number
+const umur= 16
+
+//bolean
+const amarried = true
+
+//undefined(tipe data yg belom di definisikan) bisa pakai let atau var
+let x;
+x ="fajri"
+
+//null (tipe data yg menyimpan nilai kosong)
+const org = null
+
+//simbol
+
+const simbol = Symbol("fajri gans")
+
+
+//bigint
+const bigint2 =222522444n
+
+
+
+// typ data refference(type data object)
+
+//object
+const person = {
+    name: "fajri",
+    age: 16
+}
+
+//array
+const hbi = ["mancing", "main"]
+
+//function
+
+function hai(){
+    return "hai semuanya"
+}
+
+const output = hai
+console.log (output, typeof output)
+//alt atas bawah
